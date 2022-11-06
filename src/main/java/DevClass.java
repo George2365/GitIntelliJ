daf4448c5b9381b14ce9ec404b2e28ce71293c9b
GeorgeDemoUser <117570751+GeorgeDemoUser@users.noreply.github.com>
2022-11-06 12:00:25 +0100

    Update DevClass.java (#3)
    String update

--- src/main/java/DevClass.java
+++ src/main/java/DevClass.java
@@ -1,6 +1,6 @@
 public class DevClass {
     public static void main(String[] args) {
-        System.out.print("DevClass");
+        System.out.print("DevClass123");
         //George added this
         //GeorgeDemoUser added this
     }
